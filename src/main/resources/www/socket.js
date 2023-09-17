b274a5548ab16ab4092b782ea1ff2c5995ec6c87
Mendel Feygelson <mfeygelson@gmail.com>
2023-09-16 21:48:14 -0500

    Reconnect websocket on close

--- src/main/resources/www/socket.js
+++ src/main/resources/www/socket.js
@@ -7,9 +7,25 @@ const protocol = location.protocol === "https:" ? "wss:" : "ws:"
 
 export class Socket {
   constructor(path) {
-    this.ws = new WebSocket(`${protocol}${location.host}${path}${location.search}`)
+    this.path = path
     this.handlers = new Map()
     this.errorHandlers = new Map()
+    this.on("Id", ({ id }) => localStorage.setItem("id", id))
+    this.on("GetId", (_, respond) => respond({ id: localStorage.getItem("id") }))
+    this.on("GetName", (_, respond) => {
+      const name = localStorage.getItem('name')
+      name ? respond({ name }) : location.assign('/')
+    })
+    this.on("Error", (error) => {
+      console.error(error.message)
+      this.errorHandlers.get(error.type)?.(error)
+    })
+    window.addEventListener("load", () => this.connect())
+    window.addEventListener("online", () => this.connect())
+  }
+
+  connect() {
+    this.ws = new WebSocket(`${protocol}${location.host}${this.path}${location.search}`)
     this.ws.onmessage = (msg) => {
       const [type, message, id] = readMessage(msg.data)
       const handler = this.handlers.get(type)
@@ -20,16 +36,15 @@ export class Socket {
       const respond = id ? (msg => this.send(`[${id}]`, msg)) : (msg => this.send("", msg))
       handler(message, respond)
     }
-    this.on("Id", ({ id }) => localStorage.setItem("id", id))
-    this.on("GetId", (_, respond) => respond({ id: localStorage.getItem("id") }))
-    this.on("GetName", (_, respond) => {
-      const name = localStorage.getItem('name')
-      name ? respond({name}) : location.replace('/')
-    })
-    this.on("Error", (error) => {
-      console.error(error.message)
-      this.errorHandlers.get(error.type)?.(error) 
-    })
+    this.ws.onclose = () => this.reconnect()
+  }
+
+  reconnect() {
+    setTimeout(() => {
+      if (!this.backoff) this.backoff = 100;
+      else if (this.backoff < 1000) this.backoff *= 10;
+      this.connect();
+    }, this.backoff)
   }
 
   send(messageType, content) {
