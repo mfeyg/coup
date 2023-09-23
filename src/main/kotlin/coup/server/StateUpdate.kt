33f1a775811f77b034517598446bfb1941d4515d
Mendel Feygelson <mfeygelson@gmail.com>
2023-09-23 13:12:18 -0500

    Make StateUpdate invariant

--- src/main/kotlin/coup/server/StateUpdate.kt
+++ src/main/kotlin/coup/server/StateUpdate.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.serializer
 import kotlin.reflect.full.createType
 
-class StateUpdate<out State : Any>(private val state: State) : Sendable {
+class StateUpdate<State : Any>(private val state: State) : Sendable {
   override fun toFrame(): Frame {
     val encoded = Json.encodeToString(serializer(state::class.createType()), state)
     return Frame.Text("State$encoded")
