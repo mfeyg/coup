5956f21a05cad138a2049d66f5704f6be48e1812
Mendel Feygelson <mfeygelson@gmail.com>
2023-09-23 12:25:26 -0500

    Pull players and turn indicator into separate files

---
+++ src/main/resources/www/player.js
@@ -0,0 +1,29 @@
+import { html } from 'https://esm.sh/htm/preact'
+
+function Influence({ children, hidden }) {
+    if (hidden) {
+        return html`<div class="player-influence player-influence-hidden">${children}</div>`
+    } else {
+        return html`<div class="player-influence player-influence-revealed">${children}</div>`
+    }
+}
+
+function Influences({ influences, hidden }) {
+    if (typeof influences === "number") {
+        return [...new Array(influences)].map(() => html`<${Influence} hidden=${hidden} />`)
+    } else {
+        return influences.map(influence => html`<${Influence} hidden=${hidden}>${influence}<//>`)
+    }
+}
+
+export function Player({ player, current, active }) {
+    return html`
+    <div class="player ${active ? "player-turn" : ""}">
+      <div class="player-name">${current ? html`<strong>You</strong>` : player.name}</div>
+      <div class="player-isk">${player.isk}</div>
+      <div class="player-influences">
+        <${Influences} influences={player.heldInfluences} hidden />
+        <${Influences} influences={player.revealedInfluences} />
+      </div>
+    </div>`
+}
---
+++ src/main/resources/www/turnIndicator.js
@@ -0,0 +1,8 @@
+export function TurnIndicator({ thisPlayer, activePlayer }) {
+    if (!activePlayer) return null;
+    if (activePlayer.number === thisPlayer.number) {
+        return html`<div class="turn-indicator">It's your turn</div>`
+    } else {
+        return html`<div class="turn-indicator">It's ${activePlayer.name}'s turn</div>`
+    }
+}
