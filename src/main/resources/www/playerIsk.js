f4d4da68b3bda364d29b3506eabd70a136713478
Mendel Feygelson <mfeygelson@gmail.com>
2023-09-24 11:30:02 -0500

    Add isk difference indicators

--- src/main/resources/www/playerIsk.js
+++ src/main/resources/www/playerIsk.js
@@ -1,5 +1,22 @@
 import { html } from 'https://esm.sh/htm/preact'
+import { useState } from 'https://esm.sh/preact/hooks'
+
+function Difference({difference}) {
+    if (!difference) return null;
+    if (difference > 0) {
+        return html`<div class="player-isk-difference player-isk-increase">${difference}</div>`
+    } else {
+        return html`<div class="player-isk-difference player-isk-decrease">${-difference}</div>`
+    }
+}
 
 export function PlayerIsk({amount}) {
-    return  html`<div class="player-isk">${amount}</div>`
+    const [previousAmount, setPreviousAmount] = useState(null)
+    const [difference, setDifference] = useState(0)
+    if (previousAmount != null && amount != previousAmount) {
+        setDifference(amount - previousAmount);
+        setTimeout(() => setDifference(0), 1000);
+    }
+    setPreviousAmount(amount);
+    return html`<div class="player-isk">${amount}<${Difference} difference=${difference} /></div>`
 }
