5e7d10a49adca7573e78b0994b694349a60c7721
Mendel Feygelson <mfeygelson@gmail.com>
2023-09-24 21:31:13 -0500

    Improved color theory

--- src/main/kotlin/coup/server/IdGen.kt
+++ src/main/kotlin/coup/server/IdGen.kt
@@ -2,6 +2,7 @@ package coup.server
 
 import io.ktor.util.*
 import java.awt.Color
+import kotlin.experimental.xor
 
 private val alphabet = ('A'..'Z') + ('a'..'z') + ('0'..'9')
 
@@ -13,12 +14,8 @@ val newId: String get() = newId(8)
 suspend fun idColor(id: String): Color {
   val digest = Digest("SHA-256")
   digest += id.toByteArray()
-  var bytes = listOf(0, 0, 0)
-  digest.build().asSequence().chunked(3).forEach { digs ->
-    bytes = bytes.zip(digs + 0 + 0) { a, b -> a xor b.toInt() }
-  }
-  val (x, y, z) = bytes
-  return Color.getHSBColor(x / 255f, y / 255f, z / 255f)
+  val value = digest.build().reduce(Byte::xor)
+  return Color.getHSBColor(value / 255f, 0.5f, 0.95f)
 }
 
 @OptIn(ExperimentalStdlibApi::class)
