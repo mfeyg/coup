17794587ea3b1423df0255ad3ad3fca29dbb4c76
Mendel Feygelson <mfeygelson@gmail.com>
2023-09-25 17:56:55 -0500

    Delete unused messages

--- src/main/kotlin/coup/server/message/PlayerJoined.kt
+++
@@ -1,6 +0,0 @@
-package coup.server.message
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class PlayerJoined(val playerName: String) : Message
--- src/main/kotlin/coup/server/message/PlayerLeft.kt
+++
@@ -1,6 +0,0 @@
-package coup.server.message
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class PlayerLeft(val playerName: String) : Message
