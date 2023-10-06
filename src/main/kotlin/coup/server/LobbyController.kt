dafc55a757ca9906be9dd08045bd960d75342f7f
Mendel Feygelson <mfeygelson@gmail.com>
2023-10-05 23:48:06 -0500

    I really don't know what I'm doing with these lobbies

--- src/main/kotlin/coup/server/LobbyController.kt
+++ src/main/kotlin/coup/server/LobbyController.kt
@@ -8,25 +8,36 @@ import kotlin.ConcurrentModificationException
 
 class LobbyController(private val createLobby: () -> Lobby) {
   private val lobbies = WeakHashMap<Lobby, String>()
-  private val defaultLobby = newLobby()
+  private val defaultLobby = newId
+
+  private class LobbyNotFound(id: String) : ServerError("Lobby $id not found")
 
   suspend fun connect(socket: SocketConnection, id: String?, newLobby: Boolean) {
     if (newLobby) {
-      socket.send(newLobby())
-      return
+      val lobby = newLobby()
+      socket.send(lobby)
+      lobby.connect(socket)
     } else if (id == null) {
-      socket.send(defaultLobby)
-      return
+      val lobby = lobby(defaultLobby) ?: newLobby()
+      lobbies[lobby] = defaultLobby
+      socket.send(lobby)
+      lobby.connect(socket)
+    } else {
+      val lobby = lobby(id) ?: throw LobbyNotFound(id)
+      lobby.connect(socket)
     }
-    lobby(id).connect(socket)
   }
 
-  private fun newLobby() = NewLobby(newId)
+  private suspend fun SocketConnection.send(lobby: Lobby) {
+    lobbies[lobby]?.let { id -> send(NewLobby(id)) }
+  }
 
-  private fun lobby(id: String): Lobby = try {
+  private fun newLobby() = createLobby().also { lobbies[it] = newId }
+
+  private fun lobby(id: String): Lobby? = try {
     lobbies.filterValues { it == id }.keys.firstOrNull()
-      ?: createLobby().also { lobbies[it] = id }
   } catch (e: ConcurrentModificationException) {
     lobby(id)
   }
+
 }
