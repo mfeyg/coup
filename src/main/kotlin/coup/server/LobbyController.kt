81c51a62cf617701b43db186a8cc013296b37b70
Mendel Feygelson <mfeygelson@gmail.com>
2023-10-05 23:27:07 -0500

    Accept all lobby IDs

--- src/main/kotlin/coup/server/LobbyController.kt
+++ src/main/kotlin/coup/server/LobbyController.kt
@@ -7,12 +7,8 @@ import java.util.*
 import kotlin.ConcurrentModificationException
 
 class LobbyController(private val createLobby: () -> Lobby) {
-  private val defaultLobby = NewLobby(newId)
-  private val _defaultLobby = createLobby()
   private val lobbies = WeakHashMap<Lobby, String>()
-    .apply { put(_defaultLobby, defaultLobby.id) }
-
-  class LobbyNotFound(id: String) : ServerError("Lobby $id not found")
+  private val defaultLobby = newLobby()
 
   suspend fun connect(socket: SocketConnection, id: String?, newLobby: Boolean) {
     if (newLobby) {
@@ -25,13 +21,12 @@ class LobbyController(private val createLobby: () -> Lobby) {
     lobby(id).connect(socket)
   }
 
+  private fun newLobby() = NewLobby(newId)
+
   private fun lobby(id: String): Lobby = try {
     lobbies.filterValues { it == id }.keys.firstOrNull()
-      ?: throw LobbyNotFound(id)
+      ?: createLobby().also { lobbies[it] = id }
   } catch (e: ConcurrentModificationException) {
     lobby(id)
   }
-
-  private fun newLobby() =
-    NewLobby(newId).also { lobbies[createLobby()] = it.id }
 }
