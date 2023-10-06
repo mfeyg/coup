fe1ee74fff17858354ca740f47779d611e8b6fcb
Mendel Feygelson <mfeygelson@gmail.com>
2023-10-05 22:46:05 -0500

    Store lobbies as messages

--- src/main/kotlin/coup/server/LobbyController.kt
+++ src/main/kotlin/coup/server/LobbyController.kt
@@ -7,19 +7,18 @@ import java.util.*
 import kotlin.ConcurrentModificationException
 
 class LobbyController(private val createLobby: () -> Lobby) {
-  private val defaultLobby = createLobby()
-  private val defaultLobbyId = newId
+  private val defaultLobby = NewLobby(newId)
   private val lobbies = WeakHashMap<Lobby, String>()
-    .apply { put(defaultLobby, defaultLobbyId) }
+    .apply { put(createLobby(), defaultLobby.id) }
 
   class LobbyNotFound(id: String) : ServerError("Lobby $id not found")
 
   suspend fun connect(socket: SocketConnection, id: String?, newLobby: Boolean) {
     if (newLobby) {
-      socket.send(NewLobby(newLobby()))
+      socket.send(newLobby())
       return
     } else if (id == null) {
-      socket.send(NewLobby(defaultLobbyId))
+      socket.send(defaultLobby)
       return
     }
     val lobby = findLobby(id) ?: throw LobbyNotFound(id)
@@ -32,10 +31,10 @@ class LobbyController(private val createLobby: () -> Lobby) {
     findLobby(lobbyId)
   }
 
-  private fun newLobby(): String {
+  private fun newLobby(): NewLobby {
     val lobby = createLobby()
     val id = newId
     lobbies[lobby] = id
-    return id
+    return NewLobby(id)
   }
 }
