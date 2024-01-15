f5e52664b744c49f1749a01b84a7a7dfd11c47b9
Mendel Feygelson <mfeygelson@gmail.com>
2024-01-15 12:23:24 -0600

    Remove isk difference display

--- src/main/resources/www/isk.js
+++ src/main/resources/www/isk.js
@@ -1,22 +1,5 @@
 import { html } from "htm/preact"
-import { useState } from "preact/hooks"
-
-function Difference({difference}) {
-    if (!difference) return null;
-    if (difference > 0) {
-        return html`<div class="isk-difference isk-increase">${difference}</div>`
-    } else {
-        return html`<div class="isk-difference isk-decrease">${-difference}</div>`
-    }
-}
 
 export function Isk({amount}) {
-    const [previousAmount, setPreviousAmount] = useState(null)
-    const [difference, setDifference] = useState(0)
-    if (previousAmount != null && amount != previousAmount) {
-        setDifference(amount - previousAmount);
-        setTimeout(() => setDifference(0), 1000);
-    }
-    setPreviousAmount(amount);
-    return html`<div class="isk" style="--number: ${amount}">${amount} ISK<${Difference} difference=${difference} /></div>`
+    return html`<div class="isk" style="--number: ${amount}">${amount} ISK</div>`
 }
