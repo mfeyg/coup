66f5bce1eb12f3844f4d2d41bcf9c4689af27f29
Mendel Feygelson <mfeygelson@gmail.com>
2024-02-03 20:25:50 -0600

    Change Reaction to a class just for the sake of the navigation icon

--- src/main/kotlin/coup/game/Reaction.kt
+++ src/main/kotlin/coup/game/Reaction.kt
@@ -1,7 +1,7 @@
 package coup.game
 
-sealed interface Reaction {
-  data object Allow : Reaction
-  data class Block(val blocker: Player, val blockingInfluence: Influence): Reaction
-  data class Challenge(val challenger: Player): Reaction
+sealed class Reaction {
+  data object Allow : Reaction()
+  data class Block(val blocker: Player, val blockingInfluence: Influence): Reaction()
+  data class Challenge(val challenger: Player): Reaction()
 }
