f3d59bd112f0d1e36a650b2435e136da3d15cdc6
Mendel Feygelson <mfeygelson@gmail.com>
2024-02-04 14:23:53 -0600

    Make log context atomic

--- src/main/kotlin/coup/game/GameLog.kt
+++ src/main/kotlin/coup/game/GameLog.kt
@@ -4,13 +4,16 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import org.slf4j.LoggerFactory
+import java.util.concurrent.atomic.AtomicReference
 
 /** A log which collects game events. */
 class GameLog private constructor(
   private val _events: MutableStateFlow<List<Event>>,
-  private var _context: Map<String, Any> = mapOf(),
+  context: Map<String, Any> = mapOf(),
 ) {
   constructor() : this(_events = MutableStateFlow(listOf()))
+
+  private val _context = AtomicReference(context)
 
   private val logger = LoggerFactory.getLogger(this::class.java)
 
@@ -32,7 +35,7 @@ class GameLog private constructor(
 
   /** Logs the given event with optional additions context entries. */
   fun logEvent(eventType: String, block: ContextBuilder.() -> Unit = {}) {
-    val context = ContextBuilder(_context).also(block).build()
+    val context = ContextBuilder(_context.get()).also(block).build()
     with(logger.atInfo()) {
       context.forEach { (key, value) -> addKeyValue(key, value) }
       log(eventType)
@@ -42,18 +45,18 @@ class GameLog private constructor(
 
   /** Adds values to the log context of the current scope. */
   fun logContext(block: ContextBuilder.() -> Unit) {
-    _context = ContextBuilder(_context).also(block).build()
+    _context.updateAndGet { ContextBuilder(it).also(block).build() }
   }
 
   /** Adds the result of the block to the log context, if it's non-null. */
   suspend fun <T> logContext(name: String, block: suspend () -> T): T {
     val value = block()
-    value?.let { _context += name to value }
+    value?.let { _context.updateAndGet { it + (name to value) } }
     return value
   }
 
   /** Executes the block in a new log scope. Any added log context will be local to the log scope. */
   suspend fun logScope(block: suspend GameLog.() -> Unit) {
-    GameLog(_events, _context).block()
+    GameLog(_events, _context.get()).block()
   }
 }
