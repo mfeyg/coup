0219836330f81cff11dd25f44f50dd983bba6d05
Mendel Feygelson <mfeygelson@gmail.com>
2024-02-03 21:12:02 -0600

    Make Ids secure 💪

--- src/main/kotlin/coup/server/Id.kt
+++ src/main/kotlin/coup/server/Id.kt
@@ -1,12 +1,15 @@
 package coup.server
 
+import java.security.SecureRandom
+
 data class Id(val value: String) {
   companion object {
     private val alphabet = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+    private val random = SecureRandom()
 
     operator fun invoke(length: Int = 8): Id {
       return Id((1..length)
-        .map { alphabet.random() }
+        .map { alphabet[random.nextInt(alphabet.size)] }
         .joinToString(separator = ""))
     }
   }
