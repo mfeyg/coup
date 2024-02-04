55132647541b6e6e2fa59c8299feb91a41bd4db2
Mendel Feygelson <mfeygelson@gmail.com>
2024-02-04 14:04:49 -0600

    Replace hand-thrown exception with require

--- src/main/kotlin/coup/game/rules/StandardRules.kt
+++ src/main/kotlin/coup/game/rules/StandardRules.kt
@@ -32,9 +32,7 @@ class StandardRules : Ruleset {
 
   override fun setUpBoard(players: List<Player>): Board {
 
-    if (players.size > maxPlayers) {
-      throw IllegalArgumentException("Too many players!")
-    }
+    require(players.size <= maxPlayers) { "Too many players!" }
 
     val deck = Deck(influences, 3)
     deck.shuffle()
